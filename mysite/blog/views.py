8d18d1a61ec31fe69b0541d2ee97de8eef7a9339
Atillio Pinno Fetter <atilliopf@hotmail.com>
2021-07-28 22:21:25 -0300

    mostrar

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 """
 request =  o que entra (o que o navegador envia)
 response = o que o computador/server onde o programa esta responde para o navegador
 """
 # Create your views here.
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    posts = Post.objects.all()
+    return render(
+        request,
+        'blog/post_list.html',
+        {'post_list':posts}
+    )
