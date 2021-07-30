37b4e6e84880ae2b6704033d5da53777c18f3dd2
Atillio Pinno Fetter <atilliopf@hotmail.com>
2021-07-30 20:47:19 -0300

    ok

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('post/<int:pk>',views.post_detail, name='post_detail'),
     path('contacts',views.contacts, name='contacts'),
     path('about',views.about, name='about'),
+    path('add_comment/<int:pk>',views.add_comment,name='add_comment'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,26 +1,32 @@
-from django.shortcuts import get_object_or_404, render
+from .forms import CommentForm
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Post
+
 """
 request =  o que entra (o que o navegador envia)
 response = o que o computador/server onde o programa esta responde para o navegador
 """
 # Create your views here.
+
+
 def post_list(request):
     posts = Post.objects.all()
     return render(
         request,
         'blog/post_list.html',
-        {'post_list':posts}
+        {'post_list': posts}
     )
 
-def post_detail(request,pk):
-    post = get_object_or_404(Post,pk=pk)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
 
     return render(
         request,
         'blog/post_detail.html',
-        {'post':post}
+        {'post': post}
     )
+
 
 def contacts(request):
     return render(
@@ -29,9 +35,32 @@ def contacts(request):
         {}
     )
 
+
 def about(request):
     return render(
         request,
         'blog/about.html',
         {}
+    )
+
+
+def add_comment(request, pk):
+    post_obj = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        #quando o medoto da request é POST
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post_obj
+            comment.save()
+            return redirect('post_detail',pk=post_obj.pk)
+    else:
+        #quando o metodo da request é get
+        form = CommentForm()
+
+    return render(
+        request,
+        'blog/comment.html',
+        {'form': form,
+         'post': post_obj}
     )
