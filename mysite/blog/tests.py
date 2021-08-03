489ee2e657e25e25480e6435665d623e5a654358
Atillio Pinno Fetter <atilliopf@hotmail.com>
2021-08-03 15:16:13 -0300

    ok

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,3 +1,26 @@
+from typing import Text
+from django.db.models import base
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
 
 # Create your tests here.
+class PostTestCase(TestCase):
+    def setUp(self):
+        base_user= User.objects.create_user(
+            'User para testes',
+            email=None,
+            password=None
+        )
+        Post.objects.create(
+            author = base_user,
+            title='post apra teste unitario',
+            subtitle='asdas',
+            text='texto para teste'
+        )
+
+    def test_post_can_be_published(self):
+        post = Post.objects.get(title='post apra teste unitario')
+        self.assertEqual(post.published_date,None)
+        post.publish()
+        self.assertNotEqual(post.published_date,None)
